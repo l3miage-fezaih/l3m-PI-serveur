0ad01998429a48a5221465b447c3b3ac4bed22d8
antoine volozan <volozan.antoine@gmail.com>
2021-04-13 12:18:55 +0200

    mise à jour serveur

---
+++ src/main/java/com/example/Defi.java
@@ -0,0 +1,10 @@
+package com.example;
+import java.sql.Date;
+
+public class Defi {
+    public String id;
+    public String titre;
+    public Date dateDeCreation;
+    public String description;
+    public String login_Auteur;
+}
--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -2,4 +2,11 @@ package com.example;
 
 public class User {
     public String description;
+
+    public int age;
+    public String login;
+    
+    public User(){
+
+    }
 }
